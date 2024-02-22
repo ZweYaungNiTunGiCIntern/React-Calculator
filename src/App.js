14cfb5a4843d996ab41e5aac28b3c91f47daff69
ZweYaungNiTunGicIntern <zweyaungnitun@gicjp.com>
2024-02-22 10:53:03 +0630

    react calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [num1,setNum1] = useState(0);
+  const [num2,setNum2] = useState(0);
+  const [sum,setSum] = useState(0);
+  const handleNum1Change = (e) =>{
+    setNum1(Number(e.target.value));
+  }
+  const handleNum2Change = (e) =>{
+    setNum2(Number(e.target.value));
+  }
+  const calculateSum = () =>{
+    setSum(num1+num2)
+  }
+  return(
+    <div className="container text-center">
+      <h3 className="text-center mt-3 pt-3">Calculator</h3>
+      <div className="card mx-auto" style={{ width: 450 }}>
+        <div className="card-body">
+          <div className="row mt-5 mb-5 pt-5 pb-5">
+            <div className="col-md-6">
+              <label>First Number</label>
+            </div>
+            <div className="col-md-6">
+              <input type="text" value={num1} onChange={handleNum1Change} />
+            </div>
+          </div>
+          <div className="row mt-5 mb-5 pt-5 pb-5">
+            <div className="col-md-6">
+              <label>Second Number</label>
+            </div>
+            <div className="col-md-6">
+              <input type="text" value={num2} onChange={handleNum2Change} />
+            </div>
+          </div>
+          <div className="mt-5 mb-5">
+          <button onClick={calculateSum}>Calculate Sum</button>
+          </div>
+          
+          <h2>Sum: {sum}</h2>
+        </div>
+      </div>
     </div>
-  );
+  )
+  
+    
 }
 
 export default App;
